a02de43a459bd0448525771d587a39b642ee3089
Tom Royal <tom@tomroyal.com>
2019-08-19 14:17:26 +0100

    stub more

--- routes/web.php
+++ routes/web.php
@@ -24,17 +24,36 @@ use Illuminate\Http\Request;
  */
 Route::get('/', function () {
     //
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+// add task
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
 });
 
 /*
 
 
-// Add A New Task
-
-Route::post('/task', function (Request $request) {
-    //
-});
 
 // Delete An Existing Task
 
