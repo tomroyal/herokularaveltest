57352a70545320a1d1e1d1453cae879e4daa7d81
Tom Royal <tom@tomroyal.com>
2019-08-19 14:13:34 +0100

    view tasks list

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,37 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * Display All Tasks
+ */
+Route::get('/', function () {
+    //
+    return view('tasks');
+});
+
+/*
+
+
+// Add A New Task
+
+Route::post('/task', function (Request $request) {
+    //
+});
+
+// Delete An Existing Task
+
+
+Route::delete('/task/{id}', function ($id) {
+    //
+});
+
+*/
